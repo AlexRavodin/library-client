b024824a476830920370df718f227d52c0c22b07
alex.ravodin <ravodin.alex@gmail.com>
2024-12-13 08:20:52 +0300

    feat: add paging on client

---
+++ src/hooks/UseDataFetch.ts
@@ -0,0 +1,29 @@
+﻿import {useEffect, useState} from "react";
+import {AxiosError} from "axios";
+
+export const UseDataFetch = <T>(fetchingFunction: () => Promise<T | AxiosError>) => {
+    const [data, setData] = useState<T | null>(null);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState<AxiosError | null>(null);
+
+    useEffect(() => {
+        const getter = async () => {
+            try {
+                setLoading(true);
+                const result = await fetchingFunction();
+                
+                if (res as T == null){
+                    setError(null);
+                }
+                setData(res as T);
+            } catch (error) {
+                setError(error as AxiosError);
+            } finally {
+                setLoading(false);
+            }
+        };
+
+        getter();
+    }, [fetchingFunction]);
+    return {data, loading, error};
+};
